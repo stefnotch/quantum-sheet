926de21240aceda4ecde0a754d0f353c14c4dd2f
stefnotch <brandmairstefan@gmail.com>
2020-06-18 09:55:38 +0200

    Add pyodide downloader

---
+++ utils/download-pyodide.js
@@ -0,0 +1,48 @@
+import ArchiveFiles from "@shockpkg/archive-files";
+import fse from "fs-extra";
+import path from "path";
+import fetch from "node-fetch";
+import util from "util";
+import { pipeline } from "stream";
+
+const streamPipeline = util.promisify(pipeline);
+const outputDirectory = path.join(path.resolve(), "public/pyodide");
+
+(async () => {
+  const latestReleaseResponse = await fetch(
+    "https://api.github.com/repos/iodide-project/pyodide/releases/latest"
+  );
+  if (!latestReleaseResponse.ok) {
+    throw new Error(
+      `unexpected release api response ${latestReleaseResponse.statusText}\nHeaders:\n${latestReleaseResponse.headers}`
+    );
+  }
+  const latestRelease = await latestReleaseResponse.json();
+  const assetToDownload = latestRelease.assets.find((asset) =>
+    asset.name.startsWith("pyodide-build")
+  );
+  console.log(`Downloading: ${assetToDownload.url}`);
+
+  const downloadResponse = await fetch(assetToDownload.url, {
+    headers: { Accept: "application/octet-stream" },
+  });
+  if (!downloadResponse.ok) {
+    throw new Error(
+      `unexpected download response ${latestReleaseResponse.statusText}\nHeaders:\n${latestReleaseResponse.headers}`
+    );
+  }
+  await fse.remove(outputDirectory);
+  await fse.ensureDir(outputDirectory);
+  const outputPath = path.join(outputDirectory, assetToDownload.name);
+  await streamPipeline(
+    downloadResponse.body,
+    fse.createWriteStream(outputPath)
+  );
+  console.log(`Extracting ${outputPath}`);
+
+  const archive = ArchiveFiles.createArchiveByFileExtension(outputPath);
+  await archive.read(async (entry) => {
+    await entry.extract(path.join(outputDirectory, entry.path));
+  });
+  console.log("Done");
+})();
