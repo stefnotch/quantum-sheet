c73a3968cd4f027fdca09fa2db4bdb07561e41d3
stefnotch <brandmairstefan@gmail.com>
2020-06-20 13:59:55 +0200

    Update vite

--- src/cas/pyodide-cas-worker.ts
+++ src/cas/pyodide-cas-worker.ts
@@ -1,8 +1,10 @@
+import "vite";
+
 export function usePyodideWorker() {
   let worker: Worker = (window as any)["pyodide-worker"];
   if (!worker) {
     console.log("Creating pyodide worker");
-    worker = new Worker(`${process.env.BASE_URL}pyodide-webworker.js`);
+    worker = new Worker(`${import.meta.env.BASE_URL}pyodide-webworker.js`);
     (window as any)["pyodide-worker"] = worker;
   }
 
