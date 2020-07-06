65fe3f83c432972e41fc9800384db05ab3a3603a
stefnotch <brandmairstefan@gmail.com>
2020-07-06 22:26:13 +0200

    Add selection crosshair

--- src/ui/QuantumDocument.vue
+++ src/ui/QuantumDocument.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="quantum-document" ref="documentElement">
-    <selection-pointer class="selection-pointer"></selection-pointer>
+    <div class="selection-crosshair" :style="grid.gridToStyle(selection.crosshairPosition)">+</div>
     <div
       class="quantum-element"
       v-for="block in blocks"
       :key="block.id"
-      :style="{top:(block.position.y * grid.cellSize.y) + 'px'}"
+      :style="grid.gridToStyle(block.position)"
     >
       <component :is="elementTypes[block.type]"></component>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, readonly, ref } from "vue";
-import SelectionPointer from "./SelectionPointer.vue";
+import {
+  defineComponent,
+  readonly,
+  ref,
+  Ref,
+  onMounted,
+  onUnmounted,
+  watch
+} from "vue";
 
 import { useDocument, QuantumElementTypes } from "../model/document/document";
 import ExpressionElement, {
@@ -24,16 +31,55 @@ import { QuantumElementFunctions } from "../model/document/document-element";
 import { Vec2 } from "../model/document/vectors";
 
 function useGrid() {
-  const cellSize: Vec2 = { x: 20, y: 20 };
-  return { cellSize };
+  const cellSize: Vec2 = readonly({ x: 20, y: 20 });
+
+  function gridToStyle(gridPosition: Vec2) {
+    return {
+      left: gridPosition.x * cellSize.x + "px",
+      top: gridPosition.y * cellSize.y + "px"
+    };
+  }
+
+  return { cellSize, gridToStyle };
 }
 
-function useSelectionPointer(gridCellSize: Vec2) {}
+function useSelection(
+  gridCellSize: Vec2,
+  documentElement: Ref<HTMLElement | undefined>
+) {
+  const crosshairPosition = ref<Vec2>({ x: 0, y: 0 });
+
+  function pointerDown(ev: PointerEvent) {
+    if (ev.target == documentElement.value) {
+      crosshairPosition.value = {
+        x: Math.round(ev.offsetX / gridCellSize.x),
+        y: Math.round(ev.offsetY / gridCellSize.y)
+      };
+    }
+  }
+
+  onMounted(() => {
+    watch(
+      documentElement,
+      (element, oldElement) => {
+        oldElement?.removeEventListener("pointerdown", pointerDown);
+        element?.addEventListener("pointerdown", pointerDown);
+      },
+      { immediate: true }
+    );
+  });
+  onUnmounted(() => {
+    documentElement.value?.removeEventListener("pointerdown", pointerDown);
+  });
+
+  return {
+    crosshairPosition
+  };
+}
 
 export default defineComponent({
   components: {
-    ExpressionElement,
-    SelectionPointer
+    ExpressionElement
   },
   setup() {
     const elementTypes = readonly<QuantumElementTypes>({
@@ -46,11 +92,14 @@ export default defineComponent({
     const document = useDocument(elementTypes);
     const documentElement = ref<HTMLElement>();
     const grid = useGrid();
+    const selection = useSelection(grid.cellSize, documentElement);
+
     return {
       documentElement,
       blocks: document.blocks,
       elementTypes,
-      grid
+      grid,
+      selection
     };
   }
 });
@@ -66,6 +115,7 @@ export default defineComponent({
     ),
     linear-gradient(to bottom, var(--grid-color) 1px, transparent 1px);
   position: relative;
+  touch-action: none;
 
   height: 500px;
 }
@@ -74,7 +124,11 @@ export default defineComponent({
   position: absolute;
 }
 
-.selection-pointer {
+.selection-crosshair {
   position: absolute;
+  user-select: none;
+  transform: translate(-50%, -50%);
+  padding: 1px;
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
