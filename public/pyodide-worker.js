1513127908364c83fa9247c9f36b1769b75382a2
stefnotch <brandmairstefan@gmail.com>
2021-07-25 19:10:07 +0200

    Reformat worker

--- public/pyodide-worker.js
+++ public/pyodide-worker.js
@@ -8,41 +8,41 @@
  */
 
 // TODO: Use new Pyodide APIs https://pyodide.org/en/stable/project/changelog.html#version-0-17-0
-globalThis.importScripts("./pyodide/pyodide.js");
+globalThis.importScripts('./pyodide/pyodide.js')
 
 const loadPyodide = globalThis
-  .loadPyodide({ indexURL: "./pyodide/" })
-  .then(() => globalThis.pyodide.loadPackage(["mpmath", "sympy"]))
+  .loadPyodide({ indexURL: './pyodide/' })
+  .then(() => globalThis.pyodide.loadPackage(['mpmath', 'sympy']))
   .then(() =>
-    fetch("./mathjson.py")
+    fetch('./mathjson.py')
       .then((response) => response.text())
       .then((v) => {
-        globalThis.pyodide.runPython("import sys\nsys.setrecursionlimit(999)");
-        globalThis.pyodide.runPython(v);
+        globalThis.pyodide.runPython('import sys\nsys.setrecursionlimit(999)')
+        globalThis.pyodide.runPython(v)
       })
   )
   .then(() => {
     globalThis.pyodide.runPython(`import sys
 sys.setrecursionlimit(999)
-import sympy`);
+import sympy`)
   })
-  .catch((error) => console.error(error));
+  .catch((error) => console.error(error))
 
 if (globalThis.SharedWorkerGlobalScope) {
   globalThis.onconnect = (/**@type {MessageEvent} */ event) => {
-    const messagePort = event.ports[0];
+    const messagePort = event.ports[0]
     messagePort.onmessage = function (event) {
-      messagePort.postMessage(messageHandler(event));
-    };
-    loadPyodide.then(() => messagePort.postMessage({ type: "initialized" }));
-  };
+      messagePort.postMessage(messageHandler(event))
+    }
+    loadPyodide.then(() => messagePort.postMessage({ type: 'initialized' }))
+  }
 } else if (globalThis.WorkerGlobalScope) {
   globalThis.onmessage = function (event) {
-    globalThis.postMessage(messageHandler(event));
-  };
-  loadPyodide.then(() => globalThis.postMessage({ type: "initialized" }));
+    globalThis.postMessage(messageHandler(event))
+  }
+  loadPyodide.then(() => globalThis.postMessage({ type: 'initialized' }))
 } else {
-  console.error("Please use this script in a web worker or shared worker");
+  console.error('Please use this script in a web worker or shared worker')
 }
 
 /**
@@ -51,9 +51,9 @@ if (globalThis.SharedWorkerGlobalScope) {
  * @param {string} command
  */
 function wrapSympyCommand(symbols, command) {
-  const argumentNames = symbols.join(",");
-  const argumentValues = symbols.map((v) => `sympy.Symbol('${v}')`).join(",");
-  return `MathJsonPrinter().doprint((lambda ${argumentNames}: ${command})(${argumentValues}))`;
+  const argumentNames = symbols.join(',')
+  const argumentValues = symbols.map((v) => `sympy.Symbol('${v}')`).join(',')
+  return `MathJsonPrinter().doprint((lambda ${argumentNames}: ${command})(${argumentValues}))`
 }
 
 /**
@@ -63,47 +63,45 @@ function wrapSympyCommand(symbols, command) {
  */
 function messageHandler(event) {
   /** @type {WorkerMessage} */
-  const message = event.data;
-  if (!message) return;
+  const message = event.data
+  if (!message) return
 
   try {
-    let pyodideResult = undefined;
+    let pyodideResult = undefined
 
-    if (message.type == "python") {
+    if (message.type == 'python') {
       if (message.data) {
         Object.keys(message.data).forEach((key) => {
           // Set them on globalThis, so that `from js import key` works.
           // TODO: Superseded by the globals https://pyodide.org/en/stable/usage/quickstart.html
-          globalThis[key] = message.data[key];
-        });
+          globalThis[key] = message.data[key]
+        })
       }
-      pyodideResult = globalThis.pyodide.runPython(message.command);
-    } else if (message.type == "expression") {
-      pyodideResult = globalThis.pyodide.runPython(
-        wrapSympyCommand(message.symbols, message.command)
-      );
+      pyodideResult = globalThis.pyodide.runPython(message.command)
+    } else if (message.type == 'expression') {
+      pyodideResult = globalThis.pyodide.runPython(wrapSympyCommand(message.symbols, message.command))
     } else {
-      throw new Error("Unknown command type", message);
+      throw new Error('Unknown command type', message)
     }
 
     if (pyodideResult?.destroy) {
-      console.warn("Pyodide returned a proxy", pyodideResult.toString()); // Internally calls repr()
-      console.warn(Reflect.ownKeys(pyodideResult)); // Lists all properties and methods
+      console.warn('Pyodide returned a proxy', pyodideResult.toString()) // Internally calls repr()
+      console.warn(Reflect.ownKeys(pyodideResult)) // Lists all properties and methods
       //console.warn(pyodideResult.__doc__) // Shows the documentation string
-      pyodideResult.destroy();
-      pyodideResult = undefined;
+      pyodideResult.destroy()
+      pyodideResult = undefined
     }
 
     return {
-      type: "result",
+      type: 'result',
       id: message.id,
       data: pyodideResult,
-    };
+    }
   } catch (e) {
     return {
-      type: "error",
+      type: 'error',
       id: message.id,
       message: `Command ${message.command} resulted in an error ${e.message}`,
-    };
+    }
   }
 }
